c18054e9bbecef7e3ec8df8c9c2f0811668286ea
Alex Peña <pena.alex@gmail.com>
2014-07-10 13:40:58 -0700

    Adding comments to code

--- lib/fluent/plugin/in_snmptrap.rb
+++ lib/fluent/plugin/in_snmptrap.rb
@@ -1,21 +1,26 @@
 module Fluent
+# Read snmp trap messages as events in to fluentd
   class SnmpTrapInput < Input
     Plugin.register_input('snmptrap', self)
 
-    def initialize
-      super
-      require 'snmp'
-    end
-
+    # Define default configurations
     config_param :tag, :string, :default => "alert.snmptrap"
     config_param :host, :integer, :default => 0
     config_param :port, :integer, :default => 1062
     config_param :community, :string, :default => "public"
 
+    # Initialize and bring in dependencies
+    def initialize
+      super
+      require 'snmp'
+    end # def initialize
+
+    # Load internal and external configs
     def configure(conf)
       super
-    end
+    end # def configure
 
+    # Start SNMP Trap listener
     def start
       super
       m = SNMP::TrapListener.new(:Host => @host,:Port => @port) do |manager|
@@ -28,10 +33,11 @@ module Fluent
       end
       trap("INT") { m.exit }
       m.join
-    end
+    end # def start
 
+    # Stop Listener and cleanup any open connections.
     def shutdown
       m.exit
-    end
-  end
-end
+    end # def shutdown
+  end # class SnmpTrapInput
+end # module Fluent
