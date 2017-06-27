28ebe08da8a80925ccfb17f4afc88eedcd1c2d21
shingo78 <shingo.nakagawa@gmail.com>
2017-06-27 13:05:40 +0900

    add support for loading MIBs

--- lib/fluent/plugin/in_snmptrap.rb
+++ lib/fluent/plugin/in_snmptrap.rb
@@ -11,6 +11,8 @@ module Fluent
     config_param :port, :integer, :default => 1062
     config_param :community, :string, :default => "public"
     config_param :emit_event_format, :string, :default => 'jsonized'
+    config_param :mib_dir, :string, :default => nil
+    config_param :mib_modules, :string, :default => nil
 
     unless method_defined?(:router)
       define_method(:router) { Engine }
@@ -26,6 +28,13 @@ module Fluent
     def configure(conf)
       super
       @conf = conf
+      @mib_modules = @mib_modules.split(',').map{|str| str.strip} unless @mib_modules.nil?
+      @snmp_init_params = {
+        :host            => @host,
+        :port            => @port,
+        :mib_dir         => @mib_dir,
+        :mib_modules     => @mib_modules,
+      }
       @record_generator = case @emit_event_format
                           when 'jsonized'
                             Proc.new { |trap|
@@ -36,6 +45,8 @@ module Fluent
                               {
                                 'source_ip' => trap.source_ip,
                                 'enterprise' => trap.enterprise,
+                                'oid' => trap.enterprise.to_str,
+                                'name' => trap.enterprise.to_s,
                                 'agent_addr' => trap.agent_addr.to_s,
                                 'specific_trap' => trap.specific_trap,
                                 'generic_trap' => trap.generic_trap.to_s,
@@ -51,10 +62,12 @@ module Fluent
     # Start SNMP Trap listener
     def start
       super
-      @m = SNMP::TrapListener.new(:Host => @host,:Port => @port) do |manager|
+      @m = SNMP::TrapListener.new(@snmp_init_params) do |manager|
         manager.on_trap_default do |trap|
           tag = @tag
           timestamp = Engine.now
+          # trap.enterprise doesn't have MIB information
+          trap.enterprise.with_mib(manager.instance_variable_get(:@mib))
           record = @record_generator.call(trap)
           record['tags'] = {'type' => 'alert' , 'host' => trap.source_ip}
           router.emit(tag, timestamp, record)
